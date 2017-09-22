f835396d7db1134f77c97c18d70a1d81158fbd9b
Eric Fischer <enf@pobox.com>
2017-09-22 12:40:19 -0700

    Basic cat in Java

---
+++ cat.java
@@ -0,0 +1,52 @@
+import java.io.*;
+
+public class cat {
+	private static void cat(InputStream is) {
+		byte[] buf = new byte[2000];
+
+		while (true) {
+			int n = 0;
+			try {
+				n = is.read(buf);
+			} catch (IOException ioe) {
+				System.err.println(ioe.toString());
+				System.exit(1);
+			}
+
+			if (n < 0) {
+				break;
+			}
+
+			// PrintStream.write() does not throw or return errors
+			System.out.write(buf, 0, n);
+		}
+	}
+
+	public static void main(String[] argv) {
+		if (argv.length == 0) {
+			cat(System.in);
+		} else {
+			for (int i = 0; i < argv.length; i++) {
+				FileInputStream fis = null;
+
+				try {
+					fis = new FileInputStream(argv[i]);
+				} catch (FileNotFoundException e) {
+					System.err.println(e.toString());
+					System.exit(1);
+				}
+
+				cat(fis);
+
+				try {
+					fis.close();
+				} catch (IOException ioe) {
+					System.err.println(ioe.toString());
+					System.exit(1);
+				}
+			}
+		}
+
+		System.exit(0);
+	}
+}
